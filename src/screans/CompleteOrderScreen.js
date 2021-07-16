0c7facd85f0a3919a14b2c188388e8b2cb6b45e5
Zlotkowski <zlotkowskimariusz@gmail.com>
2021-07-16 09:00:36 +0200

    Update

--- src/screans/CompleteOrderScreen.js
+++ src/screans/CompleteOrderScreen.js
@@ -16,7 +16,7 @@ export default function CompleteOrderScreen(props) {
     if (order.orderItems.length > 0) {
       createOrder(dispatch, order);
     }
-  }, [order]);
+  }, [order, dispatch]);
 
   return (
     <Box className={[styles.root, styles.navy]}>
